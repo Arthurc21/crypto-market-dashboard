36e943dd8cdf389610fe18bbbc222420921b1466
Arturo Rivera <artur.c2119@gmail.com>
2022-01-18 03:41:25 -0600

    feat: improved dashboard visuals & func

--- src/components/Dashboard/Dashboard.tsx
+++ src/components/Dashboard/Dashboard.tsx
@@ -5,6 +5,7 @@ import _ from 'lodash'
 import { CoinDataHistorical } from '../../types/CoinData'
 import { MarketDataHistorical } from '../../types/MarketData'
 import { DashboardHeader } from '../DashboardHeader/DashboardHeader'
+import { Tab } from '../Tab/Tab'
 
 interface DashboardProps {
 	historicalData: CoinDataHistorical
@@ -15,41 +16,48 @@ export const Dashboard = ({ historicalData, displayData = false }: DashboardProp
 	const [currentCoin, setCurrentCoin] = useState('BTC')
 	const [marketData, setMarketData] = useState<MarketDataHistorical>({})
 	const [availableMarkets, setAvailableMarkets] = useState<Array<string>>([])
+	const [availableCoins, setAvailableCoins] = useState<Array<string>>([])
 
 	useEffect(() => {
-		let markets: Array<string> = []
-		_.forEach(historicalData[currentCoin], (key, value) => {
-			markets.push(value)
-		})
-		setAvailableMarkets(markets)
-		if (historicalData && displayData) {
+		if (historicalData) {
 			setMarketData(historicalData[currentCoin])
 		}
-	}, [historicalData])
+		setAvailableCoins(_.keys(historicalData))
+		setAvailableMarkets(_.keys(historicalData[currentCoin]))
+	}, [historicalData, currentCoin])
 
 	return (
 		<div className="dashboard">
 			<DashboardHeader />
-			<h2>Dashboard</h2>
-			<div className="buttons">
-				<button onClick={() => setCurrentCoin('BTC')}>BTC</button>
-				<button onClick={() => setCurrentCoin('ETH')}>ETH</button>
-				<button onClick={() => setCurrentCoin('XRP')}>XRP</button>
-			</div>
-			<h2>{currentCoin}</h2>
-			{displayData && (
-				<div className="market-card-container">
-					{availableMarkets.map((market) => {
+			<div className="graphs-container">
+				<div className="tabs-container">
+					{availableCoins.map((coin) => {
 						return (
-							<MarketCard
-								key={`market-card-${currentCoin}-${market}`}
-								market={market}
-								lastPrices={marketData[market]}
+							<Tab
+								key={`tab-${coin}`}
+								text={coin}
+								value={coin}
+								onClick={(value) => setCurrentCoin(value)}
 							/>
 						)
 					})}
 				</div>
-			)}
+				<div className="card-list">
+					{displayData && (
+						<div className="market-card-container">
+							{availableMarkets.map((market) => {
+								return (
+									<MarketCard
+										key={`market-card-${currentCoin}-${market}`}
+										market={market}
+										lastPrices={marketData[market]}
+									/>
+								)
+							})}
+						</div>
+					)}
+				</div>
+			</div>
 		</div>
 	)
 }
