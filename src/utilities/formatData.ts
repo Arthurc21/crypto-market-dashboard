eb678530271a9068ea2e874d05337f6257416767
Arturo Rivera <artur.c2119@gmail.com>
2022-01-18 15:54:16 -0600

    fix: fixed formatData

--- src/utilities/formatData.ts
+++ src/utilities/formatData.ts
@@ -2,13 +2,13 @@ import { TickerData } from '../types/TickerData'
 import _ from 'lodash'
 
 export const formatData = (data: TickerData, conversionRate: number, prevData?: TickerData) => {
-	const newData = { ...data }
+	const newData = _.cloneDeep(data)
 	let date = new Date()
 	let timestamp = date.getTime()
 	newData.lastUpdate = timestamp
 	if (data.price === undefined) {
 		if (prevData && prevData.price) {
-			newData.price = prevData.price * conversionRate
+			newData.price = prevData.price
 		} else newData.price = 0
 	} else {
 		newData.price = data.price * conversionRate
